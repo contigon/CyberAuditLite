8d8b9aca5d752b4eef6739e8a5ce15f4ea6259f4
maros17 <72386027+maros17@users.noreply.github.com>
2021-08-24 09:04:43 +0300

    It works

--- MakePackage.ps1
+++ MakePackage.ps1
@@ -1,7 +1,8 @@
-start-Transcript -path $PSScriptRoot\MakePackage.Log -Force -append
 #Import-Module $PSScriptRoot\CyberFunctions.psm1
 
 function Get-Tools {
+    start-Transcript -path $PSScriptRoot\MakePackage.Log -Force -append
+    $Root = Get-Folder
     $ToolsList = @("Goddi", "NTDSAudit", "PingCastle", "Testimo", "CyberAuditLiteFD", "CyberFunctions")
     foreach ($ToolName in $ToolsList) {
         $NeedToExpand = $false
@@ -16,7 +17,7 @@ function Get-Tools {
                 $ToolURL = "https://github.com/vletoux/pingcastle/releases/download/2.10.0.0/PingCastle_2.10.0.0.zip" 
                 $NeedToExpand = $true
             }
-            "CyberAuditLiteFD"{
+            "CyberAuditLiteFD" {
                 $ToolURL = "https://github.com/maros17/GoGetCyberAuditLite/blob/main/CyberAuditLiteFD.ps1" 
             }
             "CyberFunctions" {
@@ -25,14 +26,15 @@ function Get-Tools {
             Default {}
         }
         $ToolEXEName = Split-Path $ToolURL -Leaf
-        if ($ToolEXEName -notmatch "*.ps"){
-            New-Item  -Path "$PSScriptRoot\$ToolName"  -ItemType "Directory" -Force | out-null
-            $ToolLocalPath = "$PSScriptRoot\$ToolName\$ToolEXEName"
-        }else {
-            $ToolLocalPath = "$PSScriptRoot\$ToolEXEName"            
+        if ($ToolEXEName -notmatch "*.ps") {
+            New-Item  -Path "$Root\$ToolName"  -ItemType "Directory" -Force | out-null
+            $ToolLocalPath = "$Root\$ToolName\$ToolEXEName"
+        } else {
+            $ToolLocalPath = "$Root\$ToolEXEName"            
         }
+        Write-Host "Downloading $ToolName to $ToolLocalPath" -ForegroundColor Magenta
         dl $ToolURL $ToolLocalPath
-        if ($NeedToExpand){
+        if ($NeedToExpand) {
             $ToolDirectory = Split-Path $ToolLocalPath -Parent
             Expand-Archive -Path $ToolLocalPath -DestinationPath $ToolDirectory -Force
             Remove-Item $ToolLocalPath
@@ -44,10 +46,26 @@ function Compress-All {
     
 }
 function dl($url, $to) {
-    $wc = New-Object Net.Webclient
-    $wc.headers.add('Referer', (strip_filename $url))
-    $wc.Headers.Add('User-Agent', (Get-UserAgent))
-    $wc.downloadFile($url, $to)
+    try {
+        $wc = New-Object Net.Webclient
+        $wc.headers.add('Referer', (strip_filename $url))
+        $wc.Headers.Add('User-Agent', (Get-UserAgent))
+        $wc.downloadFile($url, $to)
+    } catch {
+        Write-Host "ERROR: Couldnt download from $url" -ForegroundColor Red
+    }
+}
+
+Function Get-Folder($initialDirectory) {
+    [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+    $FolderBrowserDialog = New-Object System.Windows.Forms.FolderBrowserDialog
+    $FolderBrowserDialog.RootFolder = 'MyComputer'
+    if ($initialDirectory) { $FolderBrowserDialog.SelectedPath = $initialDirectory }
+    $Topmost = New-Object System.Windows.Forms.Form
+    $Topmost.TopMost = $True
+    $Topmost.MinimizeBox = $True
+    [void] $FolderBrowserDialog.ShowDialog($Topmost) 
+    return $FolderBrowserDialog.SelectedPath
 }
 Get-Tools
 
